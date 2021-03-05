6366ed6140c86be7122cb8d46091bca656e9c81c
Seann Branchfield <eselbeus@gmail.com>
2021-03-05 12:57:38 -0500

    Add Redux

---
+++ src/reducers/articleReducer.js
@@ -0,0 +1,7 @@
+const initialState = []
+
+const articleReducer = (articleState = initialState, action) => {
+  return [...articleState]
+}
+
+export default articleReducer;
---
+++ src/reducers/podcastReducer.js
@@ -0,0 +1,7 @@
+const initialState = []
+
+const podcastReducer = (podcastState = initialState, action) => {
+  return [...podcastState]
+}
+
+export default podcastReducer;
